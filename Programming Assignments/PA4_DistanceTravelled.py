36dba95f7a0da8bc659fdc67d6367ccef1ccfce4
Ariadna Ayala <iamariadnaayala@gmail.com>
2019-09-18 10:05:16 -0500

    Update PA4_DistanceTravelled.py

--- Programming Assignments/PA4_DistanceTravelled.py
+++ Programming Assignments/PA4_DistanceTravelled.py
@@ -2,6 +2,8 @@
 ##  Distance Travelled 
 ##  Programmer: Ariadna Ayala
 ##  Status: Completed 
+##  The program collect user's input for speed and time the calculates the speed.
+##  Error message is printed if input is lesser or equal to zero.
 ##*************************************************************************
 g = 'y'
 while g == 'Y' or g == 'y':
@@ -10,14 +12,14 @@ while g == 'Y' or g == 'y':
     print()
     print('Time\tDistance')
     print('_______________')
-    if hrs <= 0 or mph <= 0:      ## Elliminating invalid, or negative, outputs
+    if hrs <= 0 or mph <= 0:                    ## Elliminating invalid, or negative, outputs
         print('ERROR: Hours and mph must be greater than 0')
     else:
         for t in range(hrs):
-            dist = mph * (t+1)        ## t+1 instead of time
-            print(t + 1,'\t', dist) ## Formatting to 2 dec.places
+            dist = mph * (t+1)                  ## t+1 instead of time
+            print(t + 1,'\t', dist)             ## Formatting to 2 dec.places
 
-        g = 'n'                   ## Avoiding the "nfinite loop"
+        g = 'n'                                 ## Avoiding the "nfinite loop"
         
 print('End')
 print()
